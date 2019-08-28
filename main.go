a66fbe3e50ecfa48ca445a309e2a246216b55be3
Seno Wijayanto <senowijayanto@gmail.com>
2019-08-29 06:48:55 +0700

    case 2

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// Case 1
+	// ar = [1, 2, 3] => 6
 	arr := []int{1, 2, 3}
-	fmt.Printf("Case 1: %v\n", sumArray(arr))
+	fmt.Printf("Case 1: %d\n", sumArray(arr))
+
+	// Case 2
+	// Tue 16 Jul 2019 into 2019-07-16
+	strDate := "Tue 16 Jul 2019"
+	fmt.Printf("Case 2: %s\n", dateConverter(strDate))
+
+	// Case 3
+	// if n = 5, we calculate 5 x 4 x 3 x 2 x 1 and get 120
+	fmt.Printf("Case 3: %d\n")
 }
 
 func sumArray(num []int) int {
@@ -16,3 +29,26 @@ func sumArray(num []int) int {
 	}
 	return sum
 }
+
+func dateConverter(input string) string {
+	var sliceMonth = map[string]string{
+		"Jan": "01",
+		"Feb": "02",
+		"Mar": "03",
+		"Apr": "04",
+		"May": "05",
+		"Jun": "06",
+		"Jul": "07",
+		"Aug": "08",
+		"Sep": "09",
+		"Oct": "10",
+		"Nov": "11",
+		"Des": "12",
+	}
+
+	fullDate := strings.Split(input, " ")
+	strMonth := sliceMonth[fullDate[2]]
+	return fullDate[3] + "-" + strMonth + "-" + fullDate[1]
+}
+
+func factorial([]int) int {}
