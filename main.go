8819897e65e74d1c78a4f2a8ab7b9024376bfff0
Seno Wijayanto <senowijayanto@gmail.com>
2019-08-29 06:37:29 +0700

    Case 1

--- main.go
+++ main.go
@@ -4,5 +4,15 @@ import "fmt"
 
 func main() {
 	// Case 1
-	fmt.Println("Case 1")
+	arr := []int{1, 2, 3}
+	fmt.Printf("Case 1: %v\n", sumArray(arr))
+}
+
+func sumArray(num []int) int {
+	var sum int
+
+	for i := 0; i < len(num); i++ {
+		sum += num[i]
+	}
+	return sum
 }
